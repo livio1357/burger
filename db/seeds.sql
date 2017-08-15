c93baeda4ff4409ac9f9364bb04a195bfaa2301f
livio bolzon <livio@actingstudiochicago.com>
2017-08-15 13:00:25 -0500

    db running

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+insert into burgers(burger_name, devoured, date) values("piza", true , "2017-07-15 00:00:12");
+insert into burgers(burger_name, devoured, date) values("fog", true , "2017-07-15 00:00:12");
+insert into burgers(burger_name, devoured, date) values("sssssss", true , "2017-07-15 00:00:12");
