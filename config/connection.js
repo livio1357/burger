2e53381c9bc523a0800f4975db4eecf60ff57d30
livio bolzon <livio@actingstudiochicago.com>
2017-08-15 13:36:36 -0500

    config

---
+++ config/connection.js
@@ -0,0 +1,30 @@
+// Requiring mysql package
+var mysql = require("mysql");
+
+// Setting up our connection information
+var source = {
+  localhost: {
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  }
+};
+
+
+// Creating our connection
+var connection = mysql.createConnection(source.localhost);
+
+
+// Connecting to the database.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Exporting our connection
+module.exports = connection;
---
+++ config/orm.js
@@ -0,0 +1,26 @@
+var connection = require("./connection.js");
+
+var orm = {
+  selectAll: function(tableInput) {
+    var queryString = "SELECT * FROM ??";
+    connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
+      console.log(result);
+    });
+  },
+  insertOne: function (burger_name, devoured,date) {
+    var queryString = "INSERT INTO `burgers_db`.`burgers` (burger_name , devoured, date) values(??, ?? , ??)";
+    console.log(queryString);
+    connection.query(queryString, [burger_name,devoured], function(err, result) {
+      console.log(result);
+    });
+  },
+  update: function(id) {
+    var queryString = "update `burgers_db`.`burgers` set devoured = 1 where id = ??";
+
+    connection.query(queryString, [id], function(err, result) {
+      console.log(result);
+    });
+  }
+};
+
+module.exports = orm;
