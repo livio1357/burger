36207cc054ef237ca2f17a21a824c7801a5c0afb
livio bolzon <livio@actingstudiochicago.com>
2017-08-20 21:08:33 -0500

    burger

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,48 @@
+
+var express = require("express");
+
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+  burger.selectAll(function(data) {
+    var hbsObject = {
+      burger: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/", function(req, res) {
+  cat.create([
+    "name", "devoured"
+  ], [
+    req.body.name, req.body.devoured
+  ], function() {
+    res.redirect("/");
+  });
+});
+
+router.put("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  cat.update({
+    devoured: req.body.devoured
+  }, condition, function() {
+    res.redirect("/");
+  });
+});
+
+router.delete("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  cat.delete(condition, function() {
+    res.redirect("/");
+  });
+});
+
+module.exports = router;
